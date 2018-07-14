a3459d1fe6fafbe1347199c9b5b230a553bbefc2
Rohan Paleja <rohanpaleja123@gmail.com>
2018-07-14 18:56:05 -0400

    Cleaned code by adding functions for printing intro and getting the guess.

--- main.cpp
+++ main.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuess();
 
 int main()
+{
+	PrintIntro();
+	GetGuess();
+
+	return 0;
+}
+
+void PrintIntro()
 {
 	// Introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
 	cout << endl;
+	return;
+}
 
+string GetGuess()
+{
 	// get the guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
-	getline(cin,Guess);
-
+	getline(cin, Guess);
 	// Repeat guess back do them
 	cout << "Your guess was: " << Guess << endl;
 	cout << endl;
-
-	return 0;
+	return Guess;
 }
