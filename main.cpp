98373d395c8a33b08a478a344b861b9265e0fd01
Rohan Paleja <rohanpaleja123@gmail.com>
2018-07-14 13:39:53 -0400

    Initial Commit
    -BullCowGame setup with basic introduction
    -Up to Lecture 20

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	constexpr int WORD_LENGTH = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	return 0;
+}
